085311e5bcc488089ce197928ca690afa942da40
Zapphoenix <mental.barak@gmail.com>
2026-06-12 03:00:07 +0300

    Fixed repository base path

--- svelte.config.ts
+++ svelte.config.ts
@@ -15,7 +15,7 @@ export default {
       strict: true
     }),
     paths: {
-			base: process.env.NODE_ENV === 'production' ? '/your-repository-name' : ''
-		}
+      base: process.env.NODE_ENV === 'production' ? '/romantic-gestures' : ''
+    }
   }
 };
